08a5f2ad9629c56a7cd7c31251d3867b6366d804
Alexandros Tsiouris <alet@bunker-holding.com>
2024-01-02 11:01:03 +0200

    🚀

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Index } from '@/components/pages'
 
 export default function Home() {
   return (
-    <div className='w-full h-full my-20 md:my-0 md:h-screen flex justify-center items-center'>
+    <div className='w-full h-full my-20 flex justify-center items-center'>
       <Index />
     </div>
   )
--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: "/alexandros-tsiouris-dev",
+  basePath: process.env.NODE_ENV === 'production' ?
+    "/alexandros-tsiouris-dev" :
+    undefined,
   images: {
-    unoptimized: true
-  }
+    unoptimized: true,
+    loader: 'akamai',
+    path: ''
+  },
 }
 
 module.exports = nextConfig
