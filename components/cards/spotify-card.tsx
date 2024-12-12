e02ddb65a9a5119c16c05544393bfffde5ec3ae9
Alexander Tsiouris <alexandros.tsiouris.dev@gmail.com>
2024-12-12 15:43:35 +0200

    Update spotify-card.tsx

--- components/cards/spotify-card.tsx
+++ components/cards/spotify-card.tsx
@@ -1,6 +1,6 @@
 export const SpotifyCard = () => {
   return (
-    <div className='flex w-full h-full items-center justify-center bg-[#282828] rounded-card'>
+    <div className='flex w-full h-full items-center justify-center bg-[#1f1f1f] rounded-card'>
       <section className='w-full h-full flex justify-center items-center mx-5'>
         <iframe
           className='rounded-card'
