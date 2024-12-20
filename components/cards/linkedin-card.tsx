558da7e3c0e759e61377368ff5295f18248d4d04
Alexandros Tsiouris <84045141+aleksandrostsiouris@users.noreply.github.com>
2024-12-20 20:43:54 +0200

    Link preview impl

--- components/cards/linkedin-card.tsx
+++ components/cards/linkedin-card.tsx
@@ -1,25 +1,27 @@
 import { motion } from 'framer-motion';
 import React from 'react'
 import { FaLinkedin } from "react-icons/fa";
+import { LinkPreview } from '../ui/link-preview';
 
 export const LinkedInCard = () => {
   return (
-    <motion.div
-      whileHover={{
-        rotateZ: -15,
-        transition: {
-          type: 'spring',
-          bounce: 0.7,
-        }
-      }}
-      whileTap={{
-        rotateZ: 15
-      }}
-      className='hover:cursor-pointer'
-      onMouseDown={e => e.stopPropagation()}
-      onTouchStart={e => e.stopPropagation()}
-      onClick={() => window.open("https://www.linkedin.com/in/alexandros-tsiouris-8684bb166/", "__blank")}>
-      <FaLinkedin className="w-12 h-12 dark:text-primary-foreground" />
-    </motion.div>
+    <LinkPreview url='https://www.linkedin.com/in/alexandros-tsiouris-8684bb166'>
+      <motion.div
+        whileHover={{
+          rotateZ: -15,
+          transition: {
+            type: 'spring',
+            bounce: 0.7,
+          }
+        }}
+        whileTap={{
+          rotateZ: 15
+        }}
+        className='hover:cursor-pointer'
+        onMouseDown={e => e.stopPropagation()}
+        onTouchStart={e => e.stopPropagation()}>
+        <FaLinkedin className="w-12 h-12 dark:text-primary-foreground" />
+      </motion.div>
+    </LinkPreview>
   )
 }
